23b04fc0844e93ca0d52fcee5be5e356d05f5fe7
kapilsarkar <k.sarkar81@gmail.com>
2024-08-16 08:01:41 +0530

    somepartsadded

---
+++ 1-10 QUESTIONS/index.js
@@ -0,0 +1,14 @@
+//1.What is Java script ?
+
+//Java Script is a programming language that is used for converting static web pages to interactive and dynamic web pages
+
+//A java Script engine i a program present in web browsers that executes javascript code.
+
+//2. What are client side and server side?
+
+//A client is a device, application or a software component that requests and consumes services or resources from a server.
+
+//A server is a device, computer or software application that provides services,resources or function to clients.
+
+
+ 
